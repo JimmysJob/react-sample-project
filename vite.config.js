1d5fbe8d076d5f952f50f5021be4052f595019d6
Jimmy <keepcreate06@gmail.com>
2025-12-24 20:45:32 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  // 新增以下路徑處理
+  // 可分成開發中以及產品路徑         // 產品路徑.       // g-pages專案名稱
+  base: process.env.NODE_ENV === 'production' ? '/react-sample-project/' : '/',
+
   plugins: [react()],
-})
+});
